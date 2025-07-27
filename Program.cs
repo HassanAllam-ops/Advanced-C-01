7d91bcedb6e8ba1f18ee5bb997be9cf3df06f14a
Hassan Allam <hassan309pro@gmail.com>
2025-07-28 01:58:03 +0300

    Q2 : create a generic Range<T> class that represents a range of values from a minimum value to a maximum value. The range should support basic operations such as checking if a value is within the range and determining the length of the range. Requirements: Create a generic class named Range<T> where T represents the type of values. Implement a constructor that takes the minimum and maximum values to define the range. Implement a method IsInRange(T value) that returns true if the given  value is within the range, otherwise false. Implement a method Length() that returns the length of the range (the difference between the maximum and minimum values). Note: You can assume that the type T used in the Range<T> class implements the IComparable<T> interface to allow for comparisons.

--- Program.cs
+++ Program.cs
@@ -41,5 +41,69 @@
     //    }
     //}
     #endregion
+
+    #region Q2
+    //public class Range<T> where T : IComparable<T>
+    //{
+    //    public T Minimum { get; }
+    //    public T Maximum { get; }
+    //
+    //    public Range(T minimum, T maximum)
+    //    {
+    //        if (minimum.CompareTo(maximum) > 0)
+    //        {
+    //            Console.WriteLine("Minimum should be less than or equal to Maximum.");
+    //        }
+    //
+    //        Minimum = minimum;
+    //        Maximum = maximum;
+    //    }
+    //
+    //    public bool IsRange (T Value)
+    //    {
+    //        return Value.CompareTo(Minimum) >= 0 && Value.CompareTo(Maximum) <= 0;
+    //    }
+    //
+    //    public double Length()
+    //    {
+    //        try
+    //        {
+    //            double min = Convert.ToDouble(Minimum);
+    //            double max = Convert.ToDouble(Maximum);
+    //            return max - min;
+    //        }
+    //        catch
+    //        {
+    //            Console.WriteLine("Length can only be calculated for numeric types.");
+    //            return 0;
+    //        }
+    //    }
+    //
+    //    public override string ToString()
+    //    {
+    //        return $"Range IS :  [{Minimum} To {Maximum}]";
+    //    }
+    //
+    //}
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q2
+            //var intRange = new Range<int>(5, 15);
+            //Console.WriteLine(intRange.IsRange(10));
+            //Console.WriteLine(intRange.ToString());
+            //Console.WriteLine(intRange.Length());
+            //
+            //Console.WriteLine("=====================");
+            //
+            //var doubleRange = new Range<double>(36.3 , 80.2);
+            //Console.WriteLine(doubleRange.IsRange(90));
+            //Console.WriteLine(doubleRange.ToString());
+            //Console.WriteLine(doubleRange.Length());
+            #endregion
+        }
+    }
 }
 
